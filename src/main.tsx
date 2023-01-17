beb3bdb25a098f839a984a9dc049e8e5b631d36c
mikhail <mikhailabdillahtr@gmail.com>
2023-01-18 06:26:15 +0700

    chore: install fontsource 'Poppins' and clsx for merge classname

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+import '@fontsource/poppins/400.css'
+import '@fontsource/poppins/500.css'
+import '@fontsource/poppins/600.css'
+import '@fontsource/poppins/700.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -6,5 +10,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
